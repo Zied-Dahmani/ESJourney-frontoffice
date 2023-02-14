50ecc206504bda724aec0db7d98b7d6e0899dfff
Louay labidi <louay.labidi@esprit.tn>
2023-02-14 23:40:48 +0100

    added safearea

--- lib/presentation/screens/curriculum/courses/course_detail_screen.dart
+++ lib/presentation/screens/curriculum/courses/course_detail_screen.dart
@@ -54,139 +54,144 @@ class _CourseDetailScreenState extends State<CourseDetailScreen> {
       ),
     ];
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        margin: EdgeInsets.only(
-          top: 0.01 * height,
-          right: 0.02 * width,
-          left: 0.02 * width,
-          bottom: 0.01 * height,
-        ),
-        //rounded corners
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(0.08 * width),
-        ),
-        child: Column(
-          children: <Widget>[
-            Container(
-              margin: EdgeInsets.only(
-                top: 0.015 * height,
-              ),
-              child: Text(
-                widget.course.module,
-                style: const TextStyle(
-                  fontSize: 22,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Container(
+          height: double.infinity,
+          width: double.infinity,
+          margin: EdgeInsets.only(
+            top: 0.01 * height,
+            right: 0.02 * width,
+            left: 0.02 * width,
+            bottom: 0.01 * height,
+          ),
+          //rounded corners
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(0.08 * width),
+          ),
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(
+                  top: 0.015 * height,
                 ),
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(
-                top: 0.01 * height,
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Icon(
-                    Icons.av_timer_outlined,
-                    color: Color(0xffEB4A5A),
-                    size: 40,
+                child: Text(
+                  widget.course.module,
+                  style: const TextStyle(
+                    fontSize: 22,
                   ),
-                  Text(
-                    widget.course.estimatedTime,
-                    style: const TextStyle(
-                      color: Color(0xffEB4A5A),
-                      fontSize: 22,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(
-                top: 0.01 * height,
-              ),
-              child: Image.asset(
-                "assets/images/curriculum/hangman.png",
-                scale: 2.5,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(
-                top: 0.01 * height,
-              ),
-              child: Text(
-                widget.course.title,
-                style: const TextStyle(
-                  fontSize: 22,
                 ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(
-                top: 0.01 * height,
-                bottom: 0.02 * height,
-              ),
-              child: LinearPercentIndicator(
-                animation: true,
-                lineHeight: 20,
-                animationDuration: 2000,
-                percent: 0.8,
-                center: const Text(
-                  "80/100 XP",
-                  style: TextStyle(color: Colors.white),
-                ),
-                progressColor: const Color(0xffEB4A5A),
-                barRadius: const Radius.circular(20),
-              ),
-            ),
-            Expanded(
-              child: Container(
+              Container(
                 margin: EdgeInsets.only(
                   top: 0.01 * height,
-                  right: 0.01 * width,
-                  left: 0.01 * width,
                 ),
-                child: CustomScrollView(
-                  slivers: <Widget>[
-                    SliverDynamicHeightGridView(
-                      itemCount: games.length,
-                      crossAxisCount: 2,
-                      builder: (ctx, index) {
-                        return CourseItem(
-                          height: height,
-                          width: width,
-                          imagePath: games[index].imagePath,
-                          courseTitle: games[index].title,
-                          onTap: games[index].onTap,
-                        );
-                      },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Icon(
+                      Icons.av_timer_outlined,
+                      color: Color(0xffEB4A5A),
+                      size: 40,
+                    ),
+                    Text(
+                      widget.course.estimatedTime,
+                      style: const TextStyle(
+                        color: Color(0xffEB4A5A),
+                        fontSize: 22,
+                      ),
                     ),
                   ],
                 ),
               ),
-            ),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: const Color(0xffEB4A5A),
-                borderRadius: BorderRadius.circular(0.08 * width),
+              Container(
+                margin: EdgeInsets.only(
+                  top: 0.01 * height,
+                ),
+                child: const CircleAvatar(
+                  backgroundColor: Colors.transparent,
+                  maxRadius: 100,
+                  minRadius: 80,
+                  backgroundImage: AssetImage("assets/images/curriculum/hangman.png"),
+                ),
               ),
-              padding: const EdgeInsets.all(6),
-              child: TextButton(
-                onPressed: () {},
-                child: const Text(
-                  'Quiz',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    letterSpacing: 1.2,
+              Container(
+                margin: EdgeInsets.only(
+                  top: 0.01 * height,
+                ),
+                child: Text(
+                  widget.course.title,
+                  style: const TextStyle(
+                    fontSize: 22,
                   ),
                 ),
               ),
-            ),
-          ],
+              Container(
+                margin: EdgeInsets.only(
+                  top: 0.01 * height,
+                  bottom: 0.02 * height,
+                ),
+                child: LinearPercentIndicator(
+                  animation: true,
+                  lineHeight: 20,
+                  animationDuration: 2000,
+                  percent: 0.8,
+                  center: const Text(
+                    "80/100 XP",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  progressColor: const Color(0xffEB4A5A),
+                  barRadius: const Radius.circular(20),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(
+                    top: 0.01 * height,
+                    right: 0.01 * width,
+                    left: 0.01 * width,
+                  ),
+                  child: CustomScrollView(
+                    slivers: <Widget>[
+                      SliverDynamicHeightGridView(
+                        itemCount: games.length,
+                        crossAxisCount: 2,
+                        builder: (ctx, index) {
+                          return CourseItem(
+                            height: height,
+                            width: width,
+                            imagePath: games[index].imagePath,
+                            courseTitle: games[index].title,
+                            onTap: games[index].onTap,
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: const Color(0xffEB4A5A),
+                  borderRadius: BorderRadius.circular(0.08 * width),
+                ),
+                padding: const EdgeInsets.all(6),
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Quiz',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      letterSpacing: 1.2,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
