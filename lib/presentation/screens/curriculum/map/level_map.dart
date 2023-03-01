e50ec7d41ff5c630663f08158a9e23c5669ebe08
Louay labidi <louay.labidi@esprit.tn>
2023-03-01 22:32:40 +0100

    fixed null check

--- lib/presentation/screens/curriculum/map/level_map.dart
+++ lib/presentation/screens/curriculum/map/level_map.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:game_levels_scrolling_map/game_levels_scrolling_map.dart';
-import 'package:game_levels_scrolling_map/model/point_model.dart';
 
 class MapLevelScreen extends StatefulWidget {
   const MapLevelScreen({Key? key}) : super(key: key);
@@ -11,39 +9,30 @@ class MapLevelScreen extends StatefulWidget {
 }
 
 class _MapLevelScreenState extends State<MapLevelScreen> {
-  double? backgroundImageHeight;
-  @override
-  void initState() {
-    super.initState();
-    setupBackground();
-  }
 
-  setupBackground() async {
-    final img = await rootBundle.load("assets/images/curriculum/map/map_vertical_infinity.png");
-    final decodedImage = await decodeImageFromList(img.buffer.asUint8List());
 
-    setState(() {
-      backgroundImageHeight = double.tryParse(decodedImage.height.toString());
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    const imageCount = 10;
-    return  LayoutBuilder(
-      builder: (BuildContext context, BoxConstraints constraints) {
-        return GameLevelsScrollingMap.scrollable(
-          backgroundImageWidget: Column(
-            children: List.generate(
-                imageCount, (index) => Image.asset("assets/images/curriculum/map/map_vertical_infinity.png")),
+    const imageCount = 1;
+    const imageHeight = 2436.0 * imageCount;
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      return GameLevelsScrollingMap.scrollable(
+        backgroundImageWidget: Column(
+          children: List.generate(
+            imageCount,
+            (index) => Image.asset(
+              "assets/images/curriculum/map/map_vertical_infinity.png",
+            ),
           ),
-          imageUrl: "assets/images/curriculum/map/map_vertical_infinity.png",
-          svgUrl: "assets/images/curriculum/map/map_vertical.svg",
-          direction: Axis.vertical,
-          imageHeight: backgroundImageHeight! * imageCount,
-          reverseScrolling: false,
-          width: constraints.maxWidth,
-        );
-      }
-    );
+        ),
+        imageUrl: "assets/images/curriculum/map/map_vertical_infinity.png",
+        svgUrl: "assets/images/curriculum/map/map_vertical.svg",
+        direction: Axis.vertical,
+        reverseScrolling: false,
+        imageHeight: imageHeight,
+        width: constraints.maxWidth,
+      );
+    });
   }
 }
